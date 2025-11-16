5f001a50547eae6a23a1d9ca41ee831af90b173e
Carlos-Eduardo-Araujo <carlos.eduardoarsilva@gmail.com>
2025-11-16 20:19:05 -0300

    Update service-worker.js

--- apps/web/service-worker.js
+++ apps/web/service-worker.js
@@ -1,21 +1,37 @@
-const CACHE = "bootcamp-cache-v1";
 
-const ASSETS = [
-  "./index.html",
-  "./main.js",
-  "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+const CACHE_NAME = "pwa-cache-v1";
+
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html"
 ];
 
-self.addEventListener("install", event => {
+
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedFile) => {
+      return cachedFile || fetch(event.request);
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => key !== CACHE_NAME && caches.delete(key))
+      )
+    )
   );
+  self.clients.claim();
 });
