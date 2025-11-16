5e6ce283633fa073d717918105b2014651221613
Carlos-Eduardo-Araujo <carlos.eduardoarsilva@gmail.com>
2025-11-15 21:04:52 -0300

    Create main.js

---
+++ apps/web/main.js
@@ -0,0 +1,21 @@
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/apps/web/service-worker.js")
+    .then(() => console.log("Service Worker registrado"))
+    .catch(err => console.error("Erro ao registrar SW:", err));
+}
+
+
+document.getElementById("btn").addEventListener("click", async () => {
+  const div = document.getElementById("api-response");
+
+  try {
+    
+    const res = await fetch("http://localhost:3000/api/hello");
+    const data = await res.json();
+
+    div.textContent = "API respondeu: " + data.msg;
+  } catch (e) {
+    div.textContent = "Erro ao chamar API.";
+  }
+});
