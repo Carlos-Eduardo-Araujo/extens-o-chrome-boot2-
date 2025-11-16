da92e46698c0e10e983ee468d6c76275c2365f8e
Carlos-Eduardo-Araujo <carlos.eduardoarsilva@gmail.com>
2025-11-15 21:51:48 -0300

    Update service-worker.js

--- apps/web/service-worker.js
+++ apps/web/service-worker.js
@@ -1,20 +1,18 @@
 const CACHE = "bootcamp-cache-v1";
 
 const ASSETS = [
-  "/apps/web/index.html",
-  "/apps/web/main.js",
-  "/apps/web/manifest.webmanifest",
-  "/apps/web/icons/icon-192.png",
-  "/apps/web/icons/icon-512.png"
+  "./index.html",
+  "./main.js",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
-
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE).then(cache => cache.addAll(ASSETS))
   );
 });
-
 
 self.addEventListener("fetch", event => {
   event.respondWith(
