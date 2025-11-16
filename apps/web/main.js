7a37a53b9d9a32a2da803a1a5310e2b16843bcc7
Carlos-Eduardo-Araujo <carlos.eduardoarsilva@gmail.com>
2025-11-15 21:51:07 -0300

    Update main.js

--- apps/web/main.js
+++ apps/web/main.js
@@ -1,23 +1,17 @@
-
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/apps/web/service-worker.js")
-    .then(() => console.log("Service Worker registrado"))
-    .catch(err => console.error("Erro ao registrar SW:", err));
+  navigator.serviceWorker.register("./service-worker.js")
+    .then(() => console.log("SW registrado"))
+    .catch(err => console.error(err));
 }
-
 
 document.getElementById("btn").addEventListener("click", async () => {
   const div = document.getElementById("api-response");
 
   try {
-    
-    const API = "http://localhost:3000/api/hello";
-    const res = await fetch(API);
-
+    const res = await fetch("http://localhost:3000/api/hello");
     const data = await res.json();
-
     div.textContent = "API respondeu: " + data.msg;
-  } catch (e) {
-    div.textContent = "Erro ao chamar API.";
+  } catch {
+    div.textContent = "Erro ao chamar API";
   }
 });
